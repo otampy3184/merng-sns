115447d0086d4e3e1a39f339c339e60b7284d95b
django_freeman <o.tampodo.t.18@gmail.com>
2022-05-01 21:09:19 +0900

    保護

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const server = new ApolloServer({
 mongoose
     //.connect(MONGODB, { useNewUrlParser: true})
     //謎エラー回避のためべた張り
-    .connect('mongodb+srv://merng:MRu5s9TqUCEZpJxw@cluster0.ko75m.mongodb.net/merng?retryWrites=true&w=majority', {useNewUrlParser: true})
+    //.connect('mongodb+srv://merng:MRu5s9TqUCEZpJxw@cluster0.ko75m.mongodb.net/merng?retryWrites=true&w=majority', {useNewUrlParser: true})
     .then(() => {
         console.log(`MongoDB connected`);
         return server.listen({ port: 5000 })
